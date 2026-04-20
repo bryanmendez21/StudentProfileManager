2e7baada63e3c06e603945bc3382e29b1dd200f4
bryan <bryanmendez21@icloud.com>
2026-04-20 15:46:24 -0400

    added a menu so user can pick an action to perform

--- src/main/java/com/pluralsight/StudentManager.java
+++ src/main/java/com/pluralsight/StudentManager.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class StudentManager {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        Students student1 = new Students("Bryan",213,2.7,true);
+        Students student2 = new Students("Luis", 123, 3.5, true);
+        Students student3 = new Students("Jessenia",456,3.2,false);
+
+        System.out.println("----------Welcome to student profile manager---------- ");
+        System.out.println("(1) Look up Profile");
+        System.out.println("(2) Add Course");
+        System.out.println("(3) Update Gpa");
+        System.out.println("(4) Update Enrollment");
+        System.out.println("(0) Exit");
+        System.out.print("\nPick From Options Using Numerical Values: ");
+        int options = userInput.nextInt();
+        userInput.nextLine();
+        //System.out.println(student1.profile());
+
+
+    }
+
 }
--- src/main/java/com/pluralsight/Students.java
+++ src/main/java/com/pluralsight/Students.java
@@ -1,4 +1,63 @@
 package com.pluralsight;
 
 public class Students {
+    // Variables
+    private String name;
+    private int StudentId;
+    private double gpa;
+    private boolean isEnrolled;
+
+    // Constructor
+    public Students(String name, int studentId, double gpa, boolean isEnrolled) {
+        this.name = name;
+        StudentId = studentId;
+        this.gpa = 0.0;
+        this.isEnrolled = false;
+    }
+
+
+    // name getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // student id getter and setter
+    public int getStudentId() {
+        return StudentId;
+    }
+
+    public void setStudentId(int studentId) {
+        StudentId = studentId;
+    }
+
+    // gpa getter and setter
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // is enrolled getter and setter
+    public boolean isEnrolled() {
+        return isEnrolled;
+    }
+
+    public void setEnrolled(boolean enrolled) {
+        isEnrolled = enrolled;
+    }
+
+    // Method
+    public String profile(){
+        return "name: " + name + "\nstudentId: " + getStudentId() + "\nGpa: " + gpa + "\nIs enrolled: "+ isEnrolled;
+
+    }
+
+
+
 }
